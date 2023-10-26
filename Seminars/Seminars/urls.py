15faf2ded9e9386d443939fe9f7fdac25ac83c5b
Zhanibek <astic1@mail.ru>
2023-10-26 22:45:03 +0600

    before deploy commit

--- Seminars/Seminars/urls.py
+++ Seminars/Seminars/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('sem1/', include('Sem1.urls')),
+    # path('__debug__/', include('debug_toolbar.urls')),
 ]
